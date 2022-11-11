667ed282f7a33875a259015256ff418cf5d29feb
alexissauerg <110545227+alexissauerg@users.noreply.github.com>
2022-11-11 12:40:11 -0300

    Update SQLQuery5.sql
    Cambio de orden de creacion de tablas para corregir errores de foreign keys

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,24 +1,3 @@
-CREATE DATABASE tallerMecanico
-create table purchase_details(
-	id int not null PRIMARY KEY IDENTITY(1,2),
-	quantity int not null,
-	unit_cost double not null,
-	iva double not null,
-	producto_id not null FOREIGN KEY REFERENCES  products(id),
-	purchase_id  not null FOREIGN KEY REFERENCES purchase(id)
-);
-
-
-create table products(
-	id int not null PRIMARY KEY IDENTITY (1,2),
-	name varchar(100) not null,
-	description varchar(225) not null,
-	unit_cost decimal not null,
-	iva decimal not null,
-	mark_id int not null FOREIGN KEY REFERENCES marks(id),
-	line_id int not null  FOREIGN KEY REFERENCES lines(id)
-
-);
 
 create table marks(
 	id int not null PRIMARY KEY IDENTITY(1,2),
@@ -34,6 +13,17 @@ create table deposits(
 	name varchar(200) not null
 );
 
+create table products(
+	id int not null PRIMARY KEY IDENTITY (1,2),
+	name varchar(100) not null,
+	description varchar(225) not null,
+	unit_cost decimal not null,
+	iva decimal not null,
+	mark_id int not null FOREIGN KEY REFERENCES marks(id),
+	line_id int not null  FOREIGN KEY REFERENCES lines(id)
+
+);
+
 create table stocks(
 	id int not null PRIMARY KEY IDENTITY (1,2),
 	quantity  int not null,
@@ -41,23 +31,30 @@ create table stocks(
 	deposit_if int not null FOREIGN KEY REFERENCES deposits(id)
 );
 
-create table purchases(
-	id int not null PRIMARY KEY IDENTITY(1,2),
-	dat date not null,
-	condition int not null,
-	invoice_expiration date not null ,
-	total double not null,
-	total_iva double not null,
-	provider_id int not null FOREIGN KEY REFERENCES providers (id),
-	deposit_id int not null FOREIGN KEY REFERENCES deposits(id)
-);
 create table providers(
 	id int not null PRIMARY KEY IDENTITY(1,2),
 	name varchar(255) not null , 
 	address varchar(255) not null ,
 	phone varchar(15) not null,
 	email varchar(100) not null,
-	credit_amount double not null
+	credit_amount decimal not null
+);
+
+create table purchases(
+	id int not null PRIMARY KEY IDENTITY(1,2),
+	dat date not null,
+	condition int not null,
+	invoice_expiration date not null ,
+	total decimal not null,
+	total_iva decimal not null,
+	provider_id int not null FOREIGN KEY REFERENCES providers (id),
+	deposit_id int not null FOREIGN KEY REFERENCES deposits(id)
+);
+
+create table payments_orders(
+	id int not null PRIMARY KEY IDENTITY(1,2),
+	date date not null,
+	total decimal not null
 );
 
 create table payment(
@@ -66,11 +63,11 @@ create table payment(
 	purchase_id int not null FOREIGN KEY REFERENCES purchases(id),
 	monto decimal not null 
 );
-create table payments_orders(
+
+create table pay_methods(
 	id int not null PRIMARY KEY IDENTITY(1,2),
-	date date not null,
-	total double not null
-);
+	name varchar(100) not null
+)
 
 create table pay_method_detail(
 	id int not null PRIMARY KEY IDENTITY(1,2),
@@ -79,15 +76,15 @@ create table pay_method_detail(
 	monto  decimal not null
 )
 
-create table pay_methods(
-	id int not null PRIMARY KEY IDENTITY(1,2),
-	name varchar(100) not null
+
+create table rol(id int not null PRIMARY KEY IDENTITY(1,2),
+name varchar(100) not null
 )
 
 create table persons(
 id int not null PRIMARY KEY IDENTITY(1,2),
 name varchar(100) not null,
-lastname varchar(100) not null 
+lastname varchar(100) not null,
 rol_id int  not null FOREIGN KEY REFERENCES rol(id)
 )
 
@@ -98,9 +95,3 @@ manager_id int not null FOREIGN KEY REFERENCES persons(id),
 authorization_id int not null FOREIGN KEY REFERENCES persons(id),
 date date not null 			       
 )
-
-
-create table rol(id int not null PRIMARY KEY IDENTITY(1,2),
-name varchar(100) not null
-)
-
