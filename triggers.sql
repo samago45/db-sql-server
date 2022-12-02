e175a51ce8d73f14bcf32f4ef2fe88ee588bbeaa
alexissauerg <110545227+alexissauerg@users.noreply.github.com>
2022-12-02 08:53:00 -0300

    Triggers de transferencias de deposito

--- triggers.sql
+++ triggers.sql
@@ -164,3 +164,171 @@ as begin
 	where deposit_id = @idDeposito_anterior and product_id = @idProducto_anterior
 end
 
+
+create or alter trigger t_nuevoDetalleTransferencia
+on transfer_details
+for insert
+as begin
+	
+	declare @id as int,
+			@idStock as int,
+			@idTransferencia as int,
+			@cantidad as int,
+			@cantidadStockActual as int,
+			@producto as int,
+			@DepositoDestino as int
+
+	select @id = id,
+			@idStock = stock_id,
+			@idTransferencia = deposit_transfer_id,
+			@cantidad = quantity
+	from inserted transfer_details;
+
+	select @DepositoDestino = deposit_transfers.destination_deposit_id
+	from deposit_transfers where id = @idTransferencia;
+
+	select @cantidadStockActual = quantity,
+		   @producto = product_id
+	from stocks where id = @idStock;
+
+
+	if @cantidad < 1
+	begin
+		RAISERROR('No se pueden realizar transferencias nulas o menores a 0',1,1);
+		rollback TRANSACTION;
+	end
+
+	if @cantidadStockActual < @cantidad
+	begin
+		RAISERROR('La cantidad transferida excede la cantidad de stock actual',1,1);
+		rollback TRANSACTION;
+	end
+
+	/*Actualizar stock del deposito de origen*/
+	update stocks set quantity = quantity - @cantidad where id = @idStock;
+
+	/*Actualizar o crear stock de destino*/
+	if exists(select 1 from stocks where deposit_id = @DepositoDestino and product_id = @producto)
+		begin
+				update stocks set quantity = quantity + @cantidad where deposit_id = @DepositoDestino and product_id = @producto
+		end
+	else
+		begin
+			insert into stocks(quantity, product_id, deposit_id)
+			values(@cantidad, @producto, @DepositoDestino)
+		end
+
+
+end
+
+
+
+create or alter trigger t_EditarDetalleTransferencia
+on transfer_details
+for update
+as begin
+	
+	declare @id as int,
+			@idStock as int,
+			@idTransferencia as int,
+			@cantidad as int,
+			@cantidadStockActual as int,
+			@producto as int,
+			@DepositoDestino as int
+
+	declare 
+			@idStock_anterior as int,
+			@idTransferencia_anterior as int,
+			@cantidad_anterior as int,
+			@producto_anterior as int,
+			@DepositoDestino_anterior as int
+
+	select @id = id,
+			@idStock_anterior = stock_id,
+			@idTransferencia_anterior = deposit_transfer_id,
+			@cantidad_anterior = quantity
+	from deleted transfer_details;
+
+	select @DepositoDestino_anterior = deposit_transfers.destination_deposit_id
+	from deposit_transfers where id = @idTransferencia_anterior;
+
+	select @producto_anterior = product_id
+	from stocks where id = @idStock_anterior;
+
+	select @id = id,
+			@idStock = stock_id,
+			@idTransferencia = deposit_transfer_id,
+			@cantidad = quantity
+	from inserted transfer_details;
+
+	select @DepositoDestino = deposit_transfers.destination_deposit_id
+	from deposit_transfers where id = @idTransferencia;
+
+	select @cantidadStockActual = quantity,
+		   @producto = product_id
+	from stocks where id = @idStock;
+
+	/*Volver a cargar datos en el stock inicial*/
+	update stocks set quantity = quantity + @cantidad_anterior where id = @idStock_anterior;
+
+	/*Dehacer cambios en el nuevo stock*/
+	update stocks set quantity = quantity - @cantidad_anterior where deposit_id = @DepositoDestino_anterior and product_id = @producto_anterior
+
+	if @cantidad < 1
+	begin
+		RAISERROR('No se pueden realizar transferencias nulas o menores a 0',1,1);
+		rollback TRANSACTION;
+	end
+
+	if @cantidadStockActual < @cantidad
+	begin
+		RAISERROR('La cantidad transferida excede la cantidad de stock actual',1,1);
+		rollback TRANSACTION;
+	end
+
+	/*Actualizar stock del deposito de origen*/
+	update stocks set quantity = quantity - @cantidad where id = @idStock;
+
+	/*Actualizar o crear stock de destino*/
+	if exists(select 1 from stocks where deposit_id = @DepositoDestino and product_id = @producto)
+		begin
+				update stocks set quantity = quantity + @cantidad where deposit_id = @DepositoDestino and product_id = @producto
+		end
+	else
+		begin
+			insert into stocks(quantity, product_id, deposit_id)
+			values(@cantidad, @producto, @DepositoDestino)
+		end
+end
+
+create or alter trigger t_EliminarDetalleTransferencia
+on transfer_details
+for delete
+as begin
+	declare @id as int,
+			@idStock_anterior as int,
+			@idTransferencia_anterior as int,
+			@cantidad_anterior as int,
+			@producto_anterior as int,
+			@DepositoDestino_anterior as int
+
+	select @id = id,
+			@idStock_anterior = stock_id,
+			@idTransferencia_anterior = deposit_transfer_id,
+			@cantidad_anterior = quantity
+	from deleted transfer_details;
+
+	select @DepositoDestino_anterior = deposit_transfers.destination_deposit_id
+	from deposit_transfers where id = @idTransferencia_anterior;
+
+	select @producto_anterior = product_id
+	from stocks where id = @idStock_anterior;
+
+	/*Volver a cargar datos en el stock inicial*/
+	update stocks set quantity = quantity + @cantidad_anterior where id = @idStock_anterior;
+
+	/*Dehacer cambios en el nuevo stock*/
+	update stocks set quantity = quantity - @cantidad_anterior where deposit_id = @DepositoDestino_anterior and product_id = @producto_anterior
+
+end
+
